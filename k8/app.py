bd1cf3eae57be7c44d477f7b9f6a89c497b3c9c9
W. David Li <w.david.li@gmail.com>
2019-02-23 15:02:07 -0800

    Added Vagrantfile, dummy playbook.yaml and ansible.cfg to allow vagrant to auto generate Ansible inventory files for the VMs

--- k8/app.py
+++ k8/app.py
@@ -6,5 +6,5 @@ def hello_world():
     return '<h1>Flask Dockerized For David on K8!</h1>'
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
