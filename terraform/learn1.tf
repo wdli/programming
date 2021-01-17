c193c6216d097fb8c2d4abe7fbd0b6c3c957bd89
W. David Li <w.david.li@gmail.com>
2021-01-16 19:49:21 -0800

    update

--- terraform/learn1.tf
+++ terraform/learn1.tf
@@ -6,6 +6,9 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-08d489468314a58df"
   instance_type = "t2.micro"
+  tags = {
+    Name = "TF hello world"
+  }
   # vpc_security_group_ids = ["sg-0077..."]
   # subnet_id = "subnet-923a..."
 }
