e21aa228d536cba94661b86828ce0f25922b9742
W. David Li <w.david.li@gmail.com>
2021-01-31 20:56:36 -0800

    Improved pretty printing json response

--- python/template-test.py
+++ python/template-test.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
 """A simple python script template.
+
+   To run pdb debugger in emacs
+   M-x pdb
+    python -m pdb template-test.py template-test.json
 """
 
 import os
 import sys
 import argparse
-
+import json
+import requests
 
 def main(arguments):
 
@@ -21,9 +26,20 @@ def main(arguments):
                         help="Output file", default=sys.stdout, 
                         type=argparse.FileType('w'))
 
-    args = parser.parse_args(arguments)
+    #args = parser.parse_args(arguments)
 
-    print(args)
+    print(arguments)
+
+    response = requests.get(arguments[0])
+    print("Return code: {}".format(response.status_code))
+
+    print("==== Ugly ====")
+    print(response.json())
+
+    print("==== Pretty ====")
+    json_obj = json.dumps(response.json(), indent=2)
+    print(json_obj)
+    
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
