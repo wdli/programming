375f5a82c06073387b34730d4f736c9bdd08ec8c
David Li <w.david.li@gmail.com>
2022-11-06 09:10:41 -0800

    Add learn for Go learning practice

---
+++ go/learn/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+var hello string = "Hello Learning!"
+
+// Struct and method
+type Point struct {
+	X float64
+	Y float64
+}
+
+// The following function attached to the struct Point
+// (p Point) is the receiver
+// Distance is the name
+// (q Point) is the param
+func (p Point) Distance(q Point) float64 {
+	//Note: q is farther than p
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+func main() {
+
+	fmt.Println(hello)
+	fmt.Println("# Testing the struct and method function")
+
+	p := Point{1, 2} //near point
+	q := Point{3, 4} //remote point
+
+	fmt.Println(" The distance: ", p.Distance(q))
+
+}
