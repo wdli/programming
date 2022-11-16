a8d0a3090de80489d53db3150d9343b9d0af650a
David Li <w.david.li@gmail.com>
2022-11-15 20:42:23 -0800

    Add Go interface

--- go/learn/main.go
+++ go/learn/main.go
@@ -23,6 +23,12 @@ func (p Point) Distance(q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
 
+//interface and method
+
+type Shape interface {
+	area()
+}
+
 func main() {
 
 	fmt.Println(hello)
